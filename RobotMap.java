3dcdb7b65280a4c365a8b2cdfc4f06138186bfd5
Matthew Krager <metoofarmer@yahoo.com>
2014-11-25 21:16:26 -0800

    Create and Added Drive Constants

---
+++ RobotMap.java
@@ -0,0 +1,11 @@
+
+public class RobotMap {
+    public static final int DRIVE_LEFT_1    = 1;
+    public static final int DRIVE_LEFT_2    = 2;
+    public static final int DRIVE_RIGHT_1   = 3;
+    public static final int DRIVE_RIGHT_2   = 4;
+    public static final int DRIVE_ENCODER_LEFT_A    = 1;
+    public static final int DRIVE_ENCODER_LEFT_B    = 2;
+    public static final int DRIVE_ENCODER_RIGHT_A   = 3;
+    public static final int DRIVE_ENCODER_RIGHT_B   = 4;
+}
