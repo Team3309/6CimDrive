dd6ac70e7b637997be22b74c75383f831eb85dfe
Matthew Krager <metoofarmer@yahoo.com>
2014-11-25 21:12:52 -0800

    added Basic Encoders and Victors

--- Drive.java
+++ Drive.java
@@ -1,4 +1,13 @@
 
 public class Drive {
-
+  private Victor left1;
+  private Victor left2;
+  private Victor right1;
+  private Victor right2;
+  private Encoder leftEncoder;
+  private Encoder rightEncoder;
+  
+  public Drive() {
+    
+  }
 }
