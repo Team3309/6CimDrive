72c6f7d2c474254a209067c5ffe0e34577fc5b48
Matthew Krager <metoofarmer@yahoo.com>
2014-11-26 17:01:15 -0800

    added encoder and victor initializations

--- Drive.java
+++ Drive.java
@@ -8,6 +8,30 @@ public class Drive {
   private Encoder rightEncoder;
   
   public Drive() {
+    //initialize Victors
+    left1 = new Victor(RobotMap.DRIVE_LEFT_1);
+    left2 = new Victor(RobotMap.DRIVE_LEFT_2);
+    right1 = new Victor(RobotMap.DRIVE_RIGHT_1);
+    right2 = new Victor(RobotMap.DRIVE_RIGHT_2);
     
+    //initialize Encoders
+    leftEncoder = new Encoder(RobotMap.DRIVE_ENCODER_LEFT_A, RobotMap.DRIVE_ENCODER_LEFT_B, true, CounterBase.EncodingType.k1X);
+    rightEncoder= new Encoder(RobotMap.DRIVE_ENCODER_RIGHT_A, RobotMap.DRIVE_ENCODER_RIGHT_B, false,  CounterBase.EncodingType.k1X);
+    
+  }
+  
+  public void drive(double throttle, double turn) {
+    
+    
+  }
+  
+  private void setLeft(double val) {
+    left1.set(val);
+    left2.set(val);
+  }
+  
+  private void setRight(double val) {
+    right1.set(val);
+    right2.set(val);
   }
 }
