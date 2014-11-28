c1c2901e6d129b4e813fca25525651ecf2e9bbf8
TheMkrage <TheMkrage@MatthewsPC.home>
2014-11-28 11:29:24 -0800

    added various drive methods along with gyro creation

--- src/Drive.java
+++ src/Drive.java
@@ -1,19 +1,22 @@
 
 import edu.wpi.first.wpilibj.CounterBase;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.Victor;
 
 
 public class Drive {
+    //all of the sensors and motor controllers
   private Victor left1;
   private Victor left2;
   private Victor right1;
   private Victor right2;
   private Encoder leftEncoder;
   private Encoder rightEncoder;
-  
   private modifiedGyro gyro;
   
+  //the constructor
   public Drive() {
     //initialize Victors
     left1 = new Victor(RobotMap.DRIVE_LEFT_1);
@@ -25,13 +28,31 @@ public class Drive {
     leftEncoder = new Encoder(RobotMap.DRIVE_ENCODER_LEFT_A, RobotMap.DRIVE_ENCODER_LEFT_B, true, CounterBase.EncodingType.k1X);
     rightEncoder= new Encoder(RobotMap.DRIVE_ENCODER_RIGHT_A, RobotMap.DRIVE_ENCODER_RIGHT_B, false,  CounterBase.EncodingType.k1X);
     
+    //initialize gyro
+    modifiedGyro gyro = new modifiedGyro(RobotMap.DRIVE_GYRO);
+    
+  }
+  
+  //change this to change threshold
+  private final double THRESHOLD = .1;
+  boolean gyroEnabled = false;
+  
+  public void driveHalo(double throttle, double turn) {
+    
+    
+  }
+  
+  public void driveTank(double throttle, double turn) {
+      
   }
   
   public void drive(double throttle, double turn) {
-    
-    
+      if(gyroEnabled) {
+          if (Math.abs(throttle) < THRESHOLD && Math.abs(turn) < THRESHOLD) {
+              
+          }
+      }
   }
-  
   private void setLeft(double val) {
     left1.set(val);
     left2.set(val);
@@ -42,7 +63,8 @@ public class Drive {
     right2.set(val);
   }
   
-  //this class takes care of PIDSource and PIDOutput, IN ONE CLASS OMG
+  boolean straightPidEnabled = false;
+  //this class takes care of PIDSource and PIDOutput, IN ONE CLASS OMG, and this straightPID fun is for autonomous only, so it will be unused until autonomous
   private class StraightPID implements PIDSource, PIDOutput {
     public double pidGet() {
      return (leftEncoder.get() + rightEncoder.get()) / 2;
@@ -50,7 +72,7 @@ public class Drive {
 
     public void pidWrite(double d) {
      if(straightPidEnabled){
-        drive(d, 0);
+        driveHalo(d, 0);
       }
     }
   }
