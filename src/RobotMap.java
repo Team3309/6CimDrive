c473a9f3b97644c1be0a9a3eab41914dfe81d6e5
TheMkrage <TheMkrage@MatthewsPC.home>
2014-11-28 11:26:10 -0800

    added gyro to the map

--- src/RobotMap.java
+++ src/RobotMap.java
@@ -1,5 +1,6 @@
 
 public class RobotMap {
+    //all of these are ports, change them as the robot is build
     public static final int DRIVE_LEFT_1    = 1;
     public static final int DRIVE_LEFT_2    = 2;
     public static final int DRIVE_RIGHT_1   = 3;
@@ -8,4 +9,6 @@ public class RobotMap {
     public static final int DRIVE_ENCODER_LEFT_B    = 2;
     public static final int DRIVE_ENCODER_RIGHT_A   = 3;
     public static final int DRIVE_ENCODER_RIGHT_B   = 4;
+    
+    public static final int DRIVE_GYRO = 1;
 }
