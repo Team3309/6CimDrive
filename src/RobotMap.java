29124a47429a6324909fd23526e37435b7f5bd5b
TheMkrage <TheMkrage@MatthewsPC.home>
2014-11-28 13:18:44 -0800

    added 3rd to the map

--- src/RobotMap.java
+++ src/RobotMap.java
@@ -3,8 +3,10 @@ public class RobotMap {
     //all of these are ports, change them as the robot is build
     public static final int DRIVE_LEFT_1    = 1;
     public static final int DRIVE_LEFT_2    = 2;
-    public static final int DRIVE_RIGHT_1   = 3;
-    public static final int DRIVE_RIGHT_2   = 4;
+    public static final int DRIVE_LEFT_3    = 3;
+    public static final int DRIVE_RIGHT_1   = 4;
+    public static final int DRIVE_RIGHT_2   = 5;
+    public static final int DRIVE_RIGHT_3   = 6;
     public static final int DRIVE_ENCODER_LEFT_A    = 1;
     public static final int DRIVE_ENCODER_LEFT_B    = 2;
     public static final int DRIVE_ENCODER_RIGHT_A   = 3;
