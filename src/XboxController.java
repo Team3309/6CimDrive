c0aa4ff486ee58ef5521ca601e02749b83e52b84
Matthew Krager <metoofarmer@yahoo.com>
2014-12-29 11:29:33 -0800

    Added the Xbox Controller Map to Xbox Controller class

--- src/XboxController.java
+++ src/XboxController.java
@@ -14,6 +14,36 @@ import edu.wpi.first.wpilibj.Joystick;
   **/
 public class XboxController extends GenericHID{
 
+  ////BUTTONS\\\\
+  //Base Buttons
+  public static final int BUTTON_A = 1;
+  public static final int BUTTON_B = 2;
+  public static final int BUTTON_X = 3;
+  public static final int BUTTON_Y = 4;
+  //DPAD
+  public static final int BUTTON_DPAD_UP = 5;
+  public static final int BUTTON_DPAD_DOWN = 6;
+  public static final int BUTTON_DPAD_LEFT = 7;
+  public static final int BUTTON_DPAD_RIGHT = 8;
+  //Start and Back
+  public static final int BUTTON_START = 8;
+  public static final int BUTTON_BACK = 7;
+  //Sticks
+  public static final int BUTTON_LEFT_STICK = 9;
+  public static final int BUTTON_RIGHT_STICK = 10;
+  //Bumpers and Home
+  public static final int BUTTON_LB = 5;
+  public static final int BUTTON_RB = 6;
+  public static final int BUTTON_HOME = 15;
+    
+    
+  ////AXES\\\\
+  public static final int AXIS_LEFT_X = 1;
+  public static final int AXIS_LEFT_Y = 2;
+  public static final int AXIS_TRIGGER = 3; //return value of right(RT) - left(LT)
+  public static final int AXIS_RIGHT_X = 4;
+  public static final int AXIS_RIGHT_Y = 5;
+    
   //main instance joystick being called throughout class 
   Joystick Controller;
   
