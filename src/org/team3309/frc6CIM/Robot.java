f404920e6a39c919bbf354395c3c5fc5c079a246
Matthew Krager <matthew.krager@STU-MKRAGER.student.friarsnet.servitehs.org>
2015-01-09 11:34:22 -0800

    autoFormat

--- src/org/team3309/frc6CIM/Robot.java
+++ src/org/team3309/frc6CIM/Robot.java
@@ -10,22 +10,20 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot {
-    
+
     XboxController driverController = new XboxController(1);
     XboxController operatorController = new XboxController(2);
 
-    
     private Scheduler scheduler;
     Drive mDrive;
 
     //Runs when Robot is turned on
-
     public void robotInit() {
-        
+
         scheduler = Scheduler.getInstance();
         //make drive
         mDrive = Drive.getInstance();
-        
+
         //sets it so all information about the drive will be printed repeatidly during driving
         mDrive.setPrintingDriveInfo(true);
     }
@@ -75,8 +73,12 @@ public class Robot extends IterativeRobot {
             mDrive.setLowGearOn();
         } else {
             mDrive.setHighGearOn();
-            System.out.println("High Gear Enabled");
+            if (mDrive.isPrintingDriveInfo()) {
+                System.out.println("High Gear Enabled");
+            }
         }
-        System.out.println("------------------------\n");
+        if (mDrive.isPrintingDriveInfo()) {
+            System.out.println("------------------------\n");
+        }
     }
 }
