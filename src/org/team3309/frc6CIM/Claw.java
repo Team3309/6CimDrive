258f61e68986710022ebababb6af95e32c4ed8ff
Joseph Sirna <mrflark@mrflark.com>
2015-01-09 16:35:44 -0800

    strafe and autoformat

--- src/org/team3309/frc6CIM/Claw.java
+++ src/org/team3309/frc6CIM/Claw.java
@@ -13,38 +13,40 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  * @author Friarbots
  */
 public class Claw extends Subsystem {
+
     private Victor rightClaw;
     private Victor leftClaw;
 
     private static Claw instance;
+
     public static Claw getInstance() {
-        if(instance == null) {
+        if (instance == null) {
             instance = new Claw();
         }
         return instance;
     }
-    
+
     private Claw() {
         rightClaw = new Victor(RobotMap.CLAW_RIGHTSIDE);
         leftClaw = new Victor(RobotMap.CLAW_LEFTSIDE);
-        
+
     }
-    
+
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
-    
+
     public void runClawInward() {
         rightClaw.set(1);
         leftClaw.set(1);
     }
-    
+
     public void runClawOutward() {
         rightClaw.set(-1);
         leftClaw.set(-1);
     }
-    
+
     public void stopClaw() {
         rightClaw.set(0);
         leftClaw.set(0);
